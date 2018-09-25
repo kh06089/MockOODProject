fcb5af2084fc464a4242bd3fa887ac1775e2130a
bc06255 <brian.cox07@gmail.com>
2018-09-25 18:17:24 -0400

    Update Button Style

--- OODProject/src/ProfessorGUI.java
+++ OODProject/src/ProfessorGUI.java
@@ -31,6 +31,7 @@ public class ProfessorGUI extends Application {
     private double bGroundHeight = 112.5;
     private Desktop desktop = Desktop.getDesktop();
     private String backgroundColor = "-fx-background-color: #98B4C2";
+    private String buttonStyle = " -fx-background-radius: 25px; -fx-border-color: #14336C; -fx-border-width: 3px; -fx-border-radius: 20px; -fx-background-insets: 0";
 
 
     public static void main(String[] args) {
@@ -93,21 +94,21 @@ public class ProfessorGUI extends Application {
 
         question.setPrefWidth(100);
         question.setFont(Font.font("Arial", FontWeight.BOLD, 12));
-        question.setStyle(" -fx-background-radius: 25px; -fx-border-color: #14336C; -fx-border-width: 3px; -fx-border-radius: 20px;");
+        question.setStyle(buttonStyle);
 
         email.setPrefWidth(100);
         email.setFont(Font.font("Arial", FontWeight.BOLD, 12));
-        email.setStyle(" -fx-background-radius: 25px; -fx-border-color: #14336C; -fx-border-width: 3px; -fx-border-radius: 20px;");
+        email.setStyle(buttonStyle);
 
 
         reports.setPrefWidth(100);
         reports.setFont(Font.font("Arial", FontWeight.BOLD, 12));
-        reports.setStyle(" -fx-background-radius: 25px; -fx-border-color: #14336C; -fx-border-width: 3px; -fx-border-radius: 20px;");
+        reports.setStyle(buttonStyle);
 
 
         send.setPrefWidth(100);
         send.setFont(Font.font("Arial", FontWeight.BOLD, 12));
-        send.setStyle(" -fx-background-radius: 25px; -fx-border-color: #14336C; -fx-border-width: 3px; -fx-border-radius: 20px;");
+        send.setStyle(buttonStyle);
 
 
 
@@ -178,14 +179,14 @@ public class ProfessorGUI extends Application {
                 responses.setPrefWidth(100);
                 responses.setPrefHeight(50);
                 responses.setFont(Font.font("Arial", FontWeight.BOLD, 12));
-                responses.setStyle(" -fx-background-radius: 25px; -fx-border-color: #14336C; -fx-border-width: 3px; -fx-border-radius: 20px ");
+                responses.setStyle(buttonStyle);
 
                 grades.setLayoutX(160);
                 grades.setLayoutY(20);
                 grades.setPrefWidth(100);
                 grades.setPrefHeight(50);
                 grades.setFont(Font.font("Arial", FontWeight.BOLD, 12));
-                grades.setStyle(" -fx-background-radius: 25px; -fx-border-color: #14336C; -fx-border-width: 3px; -fx-border-radius: 20px ");
+                grades.setStyle(buttonStyle);
 
 
                 rPane.getChildren().addAll(responses, grades);
@@ -213,19 +214,24 @@ public class ProfessorGUI extends Application {
                 sendOut.setPrefWidth(80);
                 sendOut.setPrefHeight(20);
                 sendOut.setFont(Font.font(14) );
+                sendOut.setStyle(buttonStyle);
 
                 Label timeLimit = new Label("Set Time Limit:");
                 timeLimit.setLayoutX(120);
                 timeLimit.setLayoutY(65);
                 timeLimit.setFont(Font.font("Arial", FontWeight.BOLD, 14));
+
                 Label qcode = new Label("Set Quiz Code:");
                 qcode.setLayoutX(120);
                 qcode.setLayoutY(160);
                 qcode.setFont(Font.font("Arial", FontWeight.BOLD, 14));
+
+
                 Label feedback = new Label("Set Feedback Option:");
                 feedback.setLayoutX(120);
                 feedback.setLayoutY(230);
                 feedback.setFont(Font.font("Arial", FontWeight.BOLD, 14));
+
                 Label dayLabel = new Label("Days:");
                 dayLabel.setLayoutX(120);
                 dayLabel.setLayoutY(90);
@@ -242,19 +248,23 @@ public class ProfessorGUI extends Application {
                 ComboBox<Integer> days = new ComboBox<Integer>(FXCollections.observableArrayList(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20));
                 days.setLayoutX(120);
                 days.setLayoutY(110);
+                days.setStyle(buttonStyle);
 
                 ComboBox<Integer> hours = new ComboBox<Integer>(FXCollections.observableArrayList(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24));
                 hours.setLayoutX(190);
                 hours.setLayoutY(110);
+                hours.setStyle(buttonStyle);
 
                 ComboBox<Integer> mins = new ComboBox<Integer>(FXCollections.observableArrayList(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59));
                 mins.setLayoutX(260);
                 mins.setLayoutY(110);
+                mins.setStyle(buttonStyle);
 
 
                 TextField quizCode = new TextField();
                 quizCode.setLayoutX(120);
                 quizCode.setLayoutY(185);
+                quizCode.setStyle(buttonStyle);
                 quizCode.setTextFormatter(new TextFormatter<String>((Change change) -> {
                     String newText = change.getControlNewText();
                     if (newText.length() > 4) {
@@ -269,6 +279,7 @@ public class ProfessorGUI extends Application {
                 RadioButton rb1 = new RadioButton("Provide feedback with all questions.");
                 rb1.setToggleGroup(group);
                 rb1.setSelected(true);
+
                 RadioButton rb2 = new RadioButton("Provide feedback with only incorrect answers.");
                 rb2.setToggleGroup(group);
                 RadioButton rb3 = new RadioButton("Provide feeedback with only correct answers.");
